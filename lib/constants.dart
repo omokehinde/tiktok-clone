b7cc1568d174b0bc283a1da33ae3ac7f8f1461cc
omokehinde <engr.omokehinde@gmail.com>
2023-03-31 13:48:00 +0100

    Initial commit

---
+++ lib/constants.dart
@@ -0,0 +1,28 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
+import 'package:tiktok_clone/controllers/auth_controller.dart';
+import 'package:tiktok_clone/views/screens/add_video_screen.dart';
+
+// pages
+const pages = [
+  Center(child: Text('Home Screen')),
+  Center(child: Text('Search Screen')),
+  AddVideoScreen(),
+  Center(child: Text('Messages Screen')),
+  Center(child: Text('Profile Screen')),
+];
+
+//Colors
+const backgroundColor = Colors.black;
+var buttonColor = Colors.red[400];
+const borderColor = Colors.grey;
+
+//  Firebasee
+var firebaseAuth = FirebaseAuth.instance;
+var firebaseStorage = FirebaseStorage.instance;
+var firestore = FirebaseFirestore.instance;
+
+// controller
+var authController = AuthController.instance;
---
+++ lib/views/screens/add_video_screen.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:tiktok_clone/constants.dart';
+
+class AddVideoScreen extends StatelessWidget {
+  const AddVideoScreen({super.key});
+
+  pickVideo(ImageSource src, BuildContext context) async {
+    final video = await ImagePicker().pickVideo(source: src);
+    if (video != null) {
+      
+    }
+  }
+
+  showOptionDialohue(BuildContext context) {
+    return showDialog(
+        context: context,
+        builder: (context) => SimpleDialog(
+              children: [
+                SimpleDialogOption(
+                  onPressed: () {},
+                  child: Row(
+                    children: const [
+                      Icon(Icons.image),
+                      Padding(
+                        padding: EdgeInsets.all(7.0),
+                        child: Text(
+                          'Gallery',
+                          style: TextStyle(fontSize: 20),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                SimpleDialogOption(
+                  onPressed: () {},
+                  child: Row(
+                    children: const [
+                      Icon(Icons.camera_alt),
+                      Padding(
+                        padding: EdgeInsets.all(7.0),
+                        child: Text(
+                          'Camera',
+                          style: TextStyle(fontSize: 20),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                SimpleDialogOption(
+                  onPressed: () {},
+                  child: Row(
+                    children: const [
+                      Icon(Icons.cancel),
+                      Padding(
+                        padding: EdgeInsets.all(7.0),
+                        child: Text(
+                          'Cancel',
+                          style: TextStyle(fontSize: 20),
+                        ),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: InkWell(
+          onTap: () => showOptionDialohue(context),
+          child: Container(
+            width: 190,
+            height: 50,
+            decoration: BoxDecoration(color: buttonColor),
+            child: const Center(
+                child: Text('Add Video',
+                    style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold))),
+          ),
+        ),
+      ),
+    );
+  }
+}
